8d0b47ed2ae125e9ed8f8ed63c38a1b1bc4162ed
Prasath <147648660+Prasath4122@users.noreply.github.com>
2024-08-13 19:16:48 +0530

    Adding source files

---
+++ queries.sql
@@ -0,0 +1,15 @@
+
+-- Queries use to create the table and their values using CRUD methods
+
+-- CREATE TABLE items (
+--   id SERIAL PRIMARY KEY,
+--   title VARCHAR(100) NOT NULL
+-- );
+
+-- SELECT * FROM items ORDER BY id ASC;
+
+-- INSERT INTO items (title) VALUES ($1), [item];
+
+-- UPDATE items SET title = ($2) WHERE id = $1, [id, item];
+
+-- DELETE FROM items WHERE id = $1, [id];
